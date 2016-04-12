79f9edba404ae69266713b9ac234f1700408d302
Stephen_Adu <572481@student.inholland.nl>
2016-04-12 19:46:58 +0200

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projectDApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the projectDApp
+ */
+angular.module('projectDApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
