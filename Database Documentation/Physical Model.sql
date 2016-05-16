2034a98517aa6d57729e2cee3bd2d53b81a7687b
Mrluigifan102 <Mrluigifan102@gmail.com>
2016-05-16 14:17:14 +0200

    Minor Changes Logical, Physical Models
    Changed grade to group, and added the "drop table if exists" lines.

--- Database Documentation/Physical Model.sql
+++ Database Documentation/Physical Model.sql
@@ -1,7 +1,15 @@
 --
+--Current Database: projectSchool
+--
+
+CREATE DATABASE projectSchool;
+USE projectSchool;
+
+--
 --Table structure for table 'account'
 --
 
+DROP TABLE IF EXISTS account;
 CREATE TABLE account {
 	username VARCHAR(50) NOT NULL,
 	'type' enum('student', 'staff', 'admin') NOT NULL DEFAULT 'student',
@@ -13,6 +21,7 @@ CREATE TABLE account {
 --Table structure for table 'subject'
 --
 
+DROP TABLE IF EXISTS subject;
 CREATE TABLE subject (
 	name VARCHAR(50) NOT NULL,
 	description VARCHAR(255) DEFAULT 'Sorry, no description for this subject is available.',
@@ -23,6 +32,7 @@ CREATE TABLE subject (
 --Table structure for table 'class'
 --
 
+DROP TABLE IF EXISTS class;
 CREATE TABLE class (
 	'group' INTEGER NOT NULL,
 	timeStart DATETIME NOT NULL,
@@ -39,6 +49,7 @@ CREATE TABLE class (
 --Table structure for table 'staff'
 --
 
+DROP TABLE IF EXISTS staff;
 CREATE TABLE staff (
 	staffID INTEGER NOT NULL,
     firstName VARCHAR(50) NOT NULL,
@@ -53,6 +64,7 @@ CREATE TABLE staff (
 -- Table structure for table 'group'
 --
 
+DROP TABLE IF EXISTS 'group';
 CREATE TABLE 'group' (
 	groupID INTEGER NOT NULL,
     course VARCHAR(200) NOT NULL,
@@ -66,6 +78,7 @@ CREATE TABLE 'group' (
 -- Table structure for table 'student'
 --
 
+DROP TABLE IF EXISTS student;
 CREATE TABLE student (
 	studentID INTEGER NOT NULL,
     firstName VARCHAR(50) NOT NULL,
@@ -84,6 +97,7 @@ CREATE TABLE student (
 -- Table structure for table 'teacher'
 --
 
+DROP TABLE IF EXISTS teacher;
 CREATE TABLE teacher (
 	teacherID INTEGER NOT NULL,
 	CONSTRAINT pk_teacher PRIMARY KEY(teacherID),
@@ -94,6 +108,7 @@ CREATE TABLE teacher (
 -- Table structure for table 'sportsTutor'
 --
 
+DROP TABLE IF EXISTS sportsTutor;
 CREATE TABLE sportsTutor (
 	tutorID INTEGER NOT NULL,
 	CONSTRAINT pk_sportsTutor PRIMARY KEY(tutorID),
@@ -104,6 +119,7 @@ CREATE TABLE sportsTutor (
 -- Table structure for table 'academicTutor'
 --
 
+DROP TABLE IF EXISTS academicTutor;
 CREATE TABLE academicTutor (
 	tutorID INTEGER NOT NULL,
 	CONSTRAINT pk_academictutor PRIMARY KEY(tutorID),
